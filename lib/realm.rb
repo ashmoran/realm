8402edfd1584c980b75fdff7fc8404eef7ec6a5e
Ash Moran <ash.moran@patchspace.co.uk>
2014-09-21 19:23:56 +0100

    Remove Object#required as Ruby 2.1 now supports mandatory kwargs

--- lib/realm.rb
+++ lib/realm.rb
@@ -1,4 +1,3 @@
-require_relative 'realm/core_ext'
 require_relative 'realm/domain'
 require_relative 'realm/event_store'
 require_relative 'realm/messaging'
--- lib/realm/core_ext/object/required.rb
+++
@@ -1,8 +0,0 @@
-class Object
-  # http://stackoverflow.com/questions/13250447/can-i-have-required-named-parameters-in-ruby-2-x
-  def required(arg)
-    raise ArgumentError.new("Required keyword argument missing: #{arg.to_sym.inspect}")
-  end
-
-  alias_method :r, :required
-end
